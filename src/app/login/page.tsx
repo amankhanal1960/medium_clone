1370a1e22a8c295986438083ef77ad67c4647cbd
Aman Khanal <amankhanal1960@gmail.com>
2024-12-15 00:17:07 +0545

    new changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,59 +3,70 @@ import React from "react";
 const LoginPage = () => {
   return (
     <div>
-      <img src="/assects/close.png" alt="close logo" className="w-4 h-4 " />
       <div className="flex items-center justify-center h-screen bg-gray-50 overflow-hidden relative">
-        {/* This div adds a semi-transparent background overlay */}
+        {/* Background overlay */}
         <div className="absolute inset-0 bg-gray-50 opacity-80 z-10"></div>
-        <div className="bg-white shadow-lg rounded-lg md:w-[670px] w-[600px] p-10 md:h-[95%] h-[100%] relative z-20">
+
+        {/* Login form */}
+        <div className="bg-white shadow-lg rounded-lg md:w-[670px] w-[600px] md:p-[38px] pt-[55px] md:h-[95%] h-[100%] relative z-20">
+          <div className="relative">
+            <img
+              src="/assects/close.png"
+              alt="Close button"
+              className="absolute top-0 right-0 w-6 h-6 cursor-pointer z-30 md:-translate-y-6 md:translate-x-6 -translate-y-9 -translate-x-3"
+            />
+          </div>
           <h2 className="text-[26px] font-normal text-black mt-[14px] mb-20 text-center">
             Welcome back.
           </h2>
 
-          <div className="flex flex-col gap-3 items-center justify-center ">
-            <button className="w-[300px] flex items-center justify-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
+          <div className="flex flex-col gap-3 items-center justify-center">
+            {/* Social buttons */}
+            <button className="relative w-[300px] flex items-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
               <img
                 src="/assects/google.png"
                 alt="Google logo"
-                className="w-4 h-4 mr-5 "
+                className="absolute left-3 w-4 h-4"
               />
-              Sign in with Google
+              <span className="flex-grow text-center">Sign in with Google</span>
             </button>
 
-            <button className="w-[300px] flex items-center justify-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
+            <button className="relative w-[300px] flex items-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
               <img
                 src="/assects/facebook.png"
                 alt="Facebook logo"
-                className="w-4 h-4 mr-3"
+                className="absolute left-3 w-4 h-4"
               />
-              Sign in with Facebook
+              <span className="flex-grow text-center">
+                Sign in with Facebook
+              </span>
             </button>
 
-            <button className="w-[300px] flex items-center justify-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
+            <button className="relative w-[300px] flex items-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
               <img
                 src="/assects/apple-logo.png"
                 alt="Apple logo"
-                className="w-4 h-4 mr-3"
+                className="absolute left-3 w-4 h-4"
               />
-              Sign in with Apple
+              <span className="flex-grow text-center">Sign in with Apple</span>
             </button>
 
-            <button className="w-[300px] flex items-center justify-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
+            <button className="relative w-[300px] flex items-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
               <img
                 src="/assects/twitter.png"
-                alt="twitter logo"
-                className="w-4 h-4 mr-3"
+                alt="Twitter logo"
+                className="absolute left-3 w-4 h-4"
               />
-              Sign in with X
+              <span className="flex-grow text-center">Sign in with X</span>
             </button>
 
-            <button className="w-[300px] flex items-center justify-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
+            <button className="relative w-[300px] flex items-center border border-black rounded-full px-4 py-[10px] text-sm font-medium text-gray-800 hover:bg-gray-100">
               <img
                 src="/assects/mail.png"
-                alt="mail logo"
-                className="w-4 h-4 mr-3"
+                alt="Mail logo"
+                className="absolute left-3 w-4 h-4"
               />
-              Sign in with email
+              <span className="flex-grow text-center">Sign in with email</span>
             </button>
           </div>
 
@@ -64,21 +75,21 @@ const LoginPage = () => {
               No account?&nbsp;
               <a
                 href="#"
-                className="text-green-600 hover:underline font-semibold"
+                className="text-green-800 hover:underline font-semibold"
               >
                 Create one
               </a>
             </p>
           </div>
 
-          <div className="mt-16 text-center text-gray-600 px-10">
-            <p className="text-xs mb-10">
+          <div className="mt-16 text-center text-xs font-light text-gray-600 px-10">
+            <p className=" mb-10">
               Forgot email or trouble signing in?&nbsp;
               <a href="#" className="underline">
                 Get help.
               </a>
             </p>
-            <p className="text-xs">
+            <p>
               Click "Sign in" to agree to Medium's&nbsp;
               <a href="#" className="underline">
                 Terms of Service
